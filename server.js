8e47fef1be40e751c35e41807ae52f9546c84d71
wongsrila <jiraphong.wongsrila@gmail.com>
2023-04-17 11:52:21 +0200

    added comments

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// Middleware
 app.use(express.static("public"));
 
+// Routes
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Socket.io
 io.on("connection", (socket) => {
   console.log("a user connected");
 
@@ -23,6 +26,7 @@ io.on("connection", (socket) => {
   });
 });
 
+// Server listener
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
