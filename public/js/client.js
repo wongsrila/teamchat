5a1179d41422dd1eff11c960f34fa729c64681af
wongsrila <jiraphong.wongsrila@gmail.com>
2023-04-17 11:36:20 +0200

    fixed on send message

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,21 @@
 let socket = io();
 
-let form = document.getElementById('form');
-let input = document.getElementById('input');
+const form = document.getElementById("form");
+const ul = document.querySelector("ul");
+const input = document.getElementById("input");
 
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
-    console.log(input.value)
-    socket.emit('message', input.value);
-    input.value = '';
+    console.log(input.value);
+    socket.emit("message", input.value);
+    input.value = "";
   }
 });
 
-socket.on('message', function(msg) {
-  var item = document.createElement('li');
-  console.log(msg)
+socket.on("message", function (msg) {
+  var item = document.createElement("li");
+  console.log(msg);
   item.textContent = msg;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
--- server.js
+++ server.js
@@ -1,31 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.on("message", (message)=> {
-console.log(message)
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  })
+  socket.on("message", (message) => {
+    console.log(message);
+  });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
-
